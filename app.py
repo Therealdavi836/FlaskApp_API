9f7dfb0c92e99839445b53603ef50c7467186094
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2025-09-03 19:11:04 -0500

    se dividen ejemplos de la API del neobancon

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # Primera practica Creacion de una API en Flask de python
 #Importamos flask, metodo jsonify para convertir a json, request para traer datos del json y math para sqrt
-import math
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
@@ -22,24 +21,3 @@ def agregar_divisas():
         "mensaje": "Divisa guardada correctamente",
         "id": str(result.inserted_id)
     }), 201
-
-#Indice por defecto
-@app.route('/')
-def index():
-    return 'Index Page'
-
-#Creando un nuevo metodo sumar 
-@app.route("/sumar")
-def sumar():
-    resultado = 2 + 2
-    return jsonify(resultado)
-
-#Recibir valor especifica el metodo POST para enviar datos
-@app.route('/recibir_valor', methods=['POST'])
-def recibir_valor():
-
-    dato = request.get_json()
-    digito = dato.get('valor')
-    numero = digito + 1
-    raiz_cuadrada = math.sqrt(numero)
-    return jsonify(raiz_cuadrada)
---
+++ ejemplos.py
@@ -0,0 +1,25 @@
+import math
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+#Indice por defecto
+@app.route('/')
+def index():
+    return 'Index Page'
+
+#Creando un nuevo metodo sumar 
+@app.route("/sumar")
+def sumar():
+    resultado = 2 + 2
+    return jsonify(resultado)
+
+#Recibir valor especifica el metodo POST para enviar datos
+@app.route('/recibir_valor', methods=['POST'])
+def recibir_valor():
+
+    dato = request.get_json()
+    digito = dato.get('valor')
+    numero = digito + 1
+    raiz_cuadrada = math.sqrt(numero)
+    return jsonify(raiz_cuadrada)
