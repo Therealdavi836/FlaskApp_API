6a0ad8cadad3576551152519f6babe307aa11cfa
Juan David Fajardo Betancourt <jfajardob@unal.edu.co>
2025-09-03 19:08:38 -0500

    se crea conexión con la base de datos noSQL y se define metodo de agregación de divisas

--- app.py
+++ app.py
@@ -2,8 +2,26 @@
 #Importamos flask, metodo jsonify para convertir a json, request para traer datos del json y math para sqrt
 import math
 from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
+client = MongoClient("mongodb://localhost:27017/")
+db = client["Divisas"]
+divisas_collection = db["Valor_Divisas"]
+
+#Metodo divisas para agregarlas en la base de datos
+@app.route('/divisas', methods=['POST'])
+def agregar_divisas():
+    data = request.get_json()
+    result = divisas_collection.insert_one({
+        "origen":data["origen"],
+        "destino":data["destino"],
+        "valor":float(data["valor"])
+    })
+    return jsonify({
+        "mensaje": "Divisa guardada correctamente",
+        "id": str(result.inserted_id)
+    }), 201
 
 #Indice por defecto
 @app.route('/')
